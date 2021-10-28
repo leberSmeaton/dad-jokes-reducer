b65845b4c37b8856cee953277a3443e472b9c208
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-10-28 16:11:30 +1100

    set initial state in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import { Heading, Button } from './Styled';
 import { getJoke, reducer } from './utils/Services';
 import Background from './images/oldman.jpeg';
 
-function App() {
+const App = () => {
+  const initialState = {
+    message: "Hi there buddy",
+    textColour: "#ffffff",
+    cardColour: "#000000"
+  }
+
+  
+
   return (
     <div>
     </div>
